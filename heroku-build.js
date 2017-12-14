78e0f2087bb6dd1ba61aa027cae0d3d1b6d7a854
Zugi Nguyen <luan.nguyen@softersource.com>
2017-12-15 00:06:29 +0700

    change package

--- heroku-build.js
+++ heroku-build.js
@@ -1,39 +1,39 @@
-'use strict';
+// 'use strict';
 
-if ('HEROKU' in process.env || ('DYNO' in process.env && process.env.HOME === '/app')){
+// if ('HEROKU' in process.env || ('DYNO' in process.env && process.env.HOME === '/app')){
 
-  const pkg = require('./package.json');
-  const ChildProcess = require('child_process');
+//   const pkg = require('./package.json');
+//   const ChildProcess = require('child_process');
 
 
-  let deps = pkg.devDependencies;
-  let packages = "";
+//   let deps = pkg.devDependencies;
+//   let packages = "";
 
-  Object.keys(deps).forEach((key) => {
-    packages += `${key}@${deps[key]} `; // note space at end to separate entries
-  });
+//   Object.keys(deps).forEach((key) => {
+//     packages += `${key}@${deps[key]} `; // note space at end to separate entries
+//   });
 
-  try {
-    console.time("install");
-    console.log("starting npm install of dev dependencies");
-    ChildProcess.execSync(`npm install ${packages}`);
-    console.timeEnd("install");
+//   try {
+//     console.time("install");
+//     console.log("starting npm install of dev dependencies");
+//     ChildProcess.execSync(`npm install ${packages}`);
+//     console.timeEnd("install");
 
-    console.time("gulp");
-    console.log("starting gulp dev");
-    ChildProcess.execSync(`gulp dev`);
-    console.timeEnd("gulp");
+//     console.time("gulp");
+//     console.log("starting gulp dev");
+//     ChildProcess.execSync(`gulp dev`);
+//     console.timeEnd("gulp");
 
-    console.time("uninstall");
-    console.log("starting npm uninstall of dev dependencies");
-    ChildProcess.execSync(`npm uninstall ${packages}`);
-    console.timeEnd("uninstall");
+//     console.time("uninstall");
+//     console.log("starting npm uninstall of dev dependencies");
+//     ChildProcess.execSync(`npm uninstall ${packages}`);
+//     console.timeEnd("uninstall");
 
     
-  }
-  catch (err) {
-    console.error(err.message);
-  }
-} else {
-  console.log("Not Heroku, skipping postinstall build");
-}
+//   }
+//   catch (err) {
+//     console.error(err.message);
+//   }
+// } else {
+//   console.log("Not Heroku, skipping postinstall build");
+// }
